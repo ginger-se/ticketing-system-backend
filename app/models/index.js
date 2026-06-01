8b9f83e098a1b6eab3dfa88159173744f696ed43
Magnani Fabiola <magnanfabiola@gmail.com>
2026-06-01 18:09:28 -0500

    typo

--- app/models/index.js
+++ app/models/index.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 });
 const db = {};
 db.Sequelize = Sequelize;
-db.sequelize = sequelize
+db.sequelize = sequelize;
 
 db.event = require("./event.model.js")(sequelize, Sequelize);
 db.reservationSeat = require("./reservationSeat.model.js")(sequelize, Sequelize);
