5f059e0410c6d36d5cf2a7f5bf9ebb6b6470c251
Magnani Fabiola <magnanfabiola@gmail.com>
2026-06-01 13:30:34 -0500

    remove recipe models and add event,reservation and reservationSeat models

--- server.js
+++ server.js
@@ -42,10 +42,6 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/auth.routes.js")(app);
-require("./app/routes/ingredient.routes")(app);
-require("./app/routes/recipe.routes")(app);
-require("./app/routes/recipeStep.routes")(app);
-require("./app/routes/recipeIngredient.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
