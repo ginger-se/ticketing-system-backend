38d537ff2dcdc9b6a4a5a62c9a0925cd24ea2082
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-06-04 18:11:29 -0500

    added usertype in auth controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -29,6 +29,7 @@ exports.login = async (req, res) => {
         lastName: user.lastName,
         id: user.id,
         token: token,
+        userType: user.userType,
       };
       res.send(userInfo);
     } catch (err) {
